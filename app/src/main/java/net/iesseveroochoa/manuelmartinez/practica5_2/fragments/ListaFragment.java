ba9e52e7fdfdc2530d1c3359328824e59d30b46d
ManuelM <descom666999@gmail.com>
2019-12-11 19:11:55 +0100

    trabajando en listfragment

--- app/src/main/java/net/iesseveroochoa/manuelmartinez/practica5_2/fragments/ListaFragment.java
+++ app/src/main/java/net/iesseveroochoa/manuelmartinez/practica5_2/fragments/ListaFragment.java
@@ -23,7 +23,7 @@ import java.util.Date;
  */
 public class ListaFragment extends Fragment {
 
-    DiarioDB db;
+
 
 
     public ListaFragment() {
@@ -38,38 +38,6 @@ public class ListaFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_lista, container, false);
     }
 
-    /**
-     * Método que devuelve un cursor con todas las tuplas de la base de datos ordenadas por el parametro pasado
-     */
-    public Cursor obtenDiario(String ordenadoPor) throws SQLiteException {
 
-        return db.query(DiarioContract.DiaDiarioEntries.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                ordenadoPor + " DESC");
-    }
-
-
-    /**
-     * Metodo que inserta en la base de datos los siguientes datos
-     */
-    public void cargaDatosPrueba() {
-        DiaDiario[] dias = {new DiaDiario(new Date("11/02/2002"),
-                5, "Examen de Lenguaje de Marcas",
-                "Los temas que entran son HTML y CSS, deberas hacer una página" +
-                        " web con la estructura típica, y contestar veinte preguntas de " +
-                        "tipo test en 30 minutos"),
-                new DiaDiario(new Date("03/28/2018"),
-                        10, "Cumpleaños de Manu",
-                        "Fiesta de cumpleaños en Chikipark" +
-                                " traer tortada del Zipi-Zape y comprar regalos en Amazon " +
-                                "lo pasaremos bien")};
-        for (DiaDiario d : dias) {
-            this.insertaDia(d);
-        }
-    }
 
 }
